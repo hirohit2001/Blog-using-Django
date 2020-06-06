46507a54f9e6aadbe3c4e326364277443b869b38
Rohit Sahu <hirohit2001@gmail.com>
2020-06-06 22:24:15 +0530

    First commit

---
+++ entries/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import HomeView, EntryView, CreateEntryView
+
+app_name = 'entries'
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='blog-home'),
+    path('entry/<int:pk>/', EntryView.as_view(), name='entry-detail'),
+    path('create_entry/', CreateEntryView.as_view(success_url='/'),
+         name='create-entry'),
+]
